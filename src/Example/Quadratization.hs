277a5134db8e604ad517c733f4033fb6c9e79b2e
Ahmed Mogahed <a.mogahed@inginne.com>
2024-11-02 09:01:09 +0200

    Update Gradient and Hessian functions to be generic in `totalCost`

--- src/Example/Quadratization.hs
+++ src/Example/Quadratization.hs
@@ -1,6 +1,11 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 
 module Example.Quadratization where
 
@@ -10,6 +15,21 @@ import Numeric.LinearAlgebra
 import Type.Basic
 import Example.TotalCost
 import Type.Player
+import Numeric.AD.Mode.Reverse (Reverse)
+import Data.Reflection (Reifies)
+import Numeric.AD.Internal.Reverse (Tape)
+import Type.Reflection (Typeable)
+import Numeric.AD.Internal.On
+import Numeric.AD.Internal.Sparse
+
+type StateHessianFunctionType f a = forall s. (Reifies s Tape, Typeable s) => Player (On (Reverse s (Sparse a))) -> f (On (Reverse s (Sparse a))) -> [On (Reverse s (Sparse a))] -> On (Reverse s (Sparse a))
+type InputHessianFunctionType f a = forall s. (Reifies s Tape, Typeable s) => Player (On (Reverse s (Sparse a))) -> [On (Reverse s (Sparse a))] -> f (On (Reverse s (Sparse a))) -> On (Reverse s (Sparse a))
+type StateGradientFunctionType f a = forall s. (Reifies s Tape, Typeable s) => Player (Reverse s a) -> f (Reverse s a) -> [Reverse s a] -> Reverse s a
+type GenericGradientFunctionType a = forall s. (Reifies s Tape, Typeable s) => Player (Reverse s a) -> [Reverse s a] -> [Reverse s a] -> Reverse s a
+type GenericHessianFunctionType a = forall s. (Reifies s Tape, Typeable s) => Player (On (Reverse s (Sparse a))) -> [On (Reverse s (Sparse a))] -> [On (Reverse s (Sparse a))] -> On (Reverse s (Sparse a))
+
+type ActiveApplyType f a = f a
+type InactiveApplyType a = [a]
 
 quadratizeCosts :: [Player R] -> StateControlData -> LinearMultiSystemCosts
 quadratizeCosts players stateControlPair = LinearMultiSystemCosts qs ls rs
@@ -18,29 +38,34 @@ quadratizeCosts players stateControlPair = LinearMultiSystemCosts qs ls rs
     x = priorState stateControlPair
     u = controlInput stateControlPair
     extractComponents player =
-      let LinearSystemCosts q l r = quadratizeCostsForPlayer x u player
+      let LinearSystemCosts q l r = quadratizeCostsForPlayer player x u
       in (q, l, r)
 
-quadratizeCostsForPlayer :: Vector R -> Vector R -> Player R -> LinearSystemCosts
-quadratizeCostsForPlayer x u player = LinearSystemCosts qs ls rs
-  where
-    qs = stateHessian x u player    
-    ls = systemStateGradient x u player
-    rs = inputHessian x u player
 
-stateHessian :: Vector R -> Vector R -> Player R -> Matrix R
-stateHessian states input player = matrix (size states) $ concat $ hessian totalCost' (toList states)
+quadratizeCostsForPlayer :: Player R -> Vector R -> Vector R -> LinearSystemCosts
+quadratizeCostsForPlayer player x u = LinearSystemCosts qs ls rs
   where
-    totalCost' x = totalCost (fmap auto player) x (map auto (toList input)) 
+    qs = matrix (size x) $ concat $ stateHessian totalCost player states inputs
+    ls = vector $ stateGradient totalCost player states inputs
+    ar = matrix (size u) $ concat $ inputHessian totalCost player states inputs
+    rs = map (\(a,b) -> ar ?? (Range a 1 b, Range a 1 b)) [(0,1),(2,3),(4,5)]
 
-inputHessian :: Vector R -> Vector R -> Player R -> [Matrix R]
-inputHessian states input player = rs
-  where
-    ar = matrix (size input) $ concat $ hessian totalCost' (toList input)
-    totalCost' u = totalCost (fmap auto player) (map auto (toList states)) u
-    rs = map (\(x,y) -> ar ?? (Range x 1 y, Range x 1 y)) [(0,1),(2,3),(4,5)]
+    states = toList x
+    inputs = toList u
 
-systemStateGradient :: Vector R -> Vector R -> Player R -> Vector R
-systemStateGradient states input player = vector $ grad totalCost' (toList states)
+-- Generic Gradient and Hessians (taking `totalcost` as argument)
+
+stateHessian :: (Traversable f, Floating a) => StateHessianFunctionType f a -> Player a -> ActiveApplyType f a -> InactiveApplyType a -> f (f a)
+stateHessian totCost player states input = hessian totalCost' states
   where
-    totalCost' x = totalCost (fmap auto player) x (map auto (toList input))
+    totalCost' x = totCost (fmap auto player) x (map auto input)
+
+inputHessian :: (Traversable f, Floating a) => InputHessianFunctionType f a -> Player a -> InactiveApplyType a -> ActiveApplyType f a -> f (f a)
+inputHessian totCost player states input = hessian totalCost' input
+  where
+    totalCost' u = totCost (fmap auto player) (map auto states) u
+
+stateGradient :: (Traversable f, Floating a) => StateGradientFunctionType f a -> Player a -> ActiveApplyType f a -> InactiveApplyType a -> ActiveApplyType f a 
+stateGradient totCost player states input = grad totalCost' states
+  where 
+    totalCost' x = totCost (fmap auto player) x (map auto input)

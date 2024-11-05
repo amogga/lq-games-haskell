729b1bf0ed936969130370440550486bf8104c20
Ahmed Mogahed <a.mogahed@inginne.com>
2024-11-05 14:55:57 +0200

    Boilerplate for main

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,5 @@
 module Main (main) where
 
-import Example.TotalCost
-import Numeric.LinearAlgebra
-import Example.Cost.ProximityCost
-import Plot.TrajectorySketch
-
 main :: IO ()
 main = do
     -- let states = vector [6.5, 0.0, pi / 2.0 , 1.0, 1.5, 40, -(pi / 2.0), 0.1, 0.0, 22.0, 0.0, 2.0]
@@ -18,8 +13,26 @@ main = do
     -- let pos2 = Position (4.4,5)
     -- createPlotViaGloss
     -- createPlot
-    let bn2 = norm_2 ((vector[1,2.3,4]) - (vector [2.2,3.4,3]))
-    let bninf = norm_Inf ((vector[1,2.3,4]) - (vector [2.2,3.4,3]))
-    print bn2
-    print bninf
-    -- putStrLn "Hello, World!"
+    -- let bn2 = norm_2 ((vector[1,2.3,4]) - (vector [2.2,3.4,3]))
+    -- let bninf = norm_Inf ((vector[1,2.3,4]) - (vector [2.2,3.4,3]))
+    -- print bn2
+    -- print bninf
+
+    print "sd"
+
+
+-- Generic Gradient and Hessians (taking `totalcost` as argument)
+-- 'stateHessianT :: (Traversable f, Floating a) => StateHessianFunctionType f a -> Player a -> ActiveApplyType f a -> InactiveApplyType a -> f (f a)
+-- stateHessianT totCost player states input = hessian totalCost' states
+--   where
+--     totalCost' x = totCost (fmap auto player) x (map auto input)
+
+-- inputHessianT :: (Traversable f, Floating a) => InputHessianFunctionType f a -> Player a -> InactiveApplyType a -> ActiveApplyType f a -> f (f a)
+-- inputHessianT totCost player states = hessian totalCost'
+--   where
+--     totalCost' u = totCost (fmap auto player) (map auto states) u'
+
+-- stateGradientT :: (Traversable f, Floating a) => StateGradientFunctionType f a -> Player a -> ActiveApplyType f a -> InactiveApplyType a -> ActiveApplyType f a
+-- stateGradientT totCost player states input = grad totalCost' states
+--   where
+--     totalCost' x = totCost (fmap auto player) x (map auto input)

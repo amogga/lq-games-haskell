ac32583f88783a9f339f8abf0e5d0ac0a796ac4d
Ahmed Mogahed <a.mogahed@inginne.com>
2024-11-16 07:47:34 +0200

    Refining example 2

--- examples/Example2/Main.hs
+++ examples/Example2/Main.hs
@@ -1,33 +1,15 @@
 module Main (main) where
 
-import Players
 import Simulation
-import TotalCost
-import Numeric.LinearAlgebra
-import TotalCost (totalCost)
-import Simulation (quadratizeCostsE1)
-import Numeric.AD 
-import Plot.CreateGifs
-import Numeric.AD.Internal.Kahn (Tape(Var))
-import Type.Basic (StateControlData(priorState))
 
 main :: IO ()
 main = do
-    let states =  vector [2.5, -10, pi / 2.0 , 5.0,
-                         -1, -10, pi / 2.0, 5.0,
-                         2.5, 10.0, pi / 2.0, 5.25]
-    let input =  vector [0,0,0,0,0,0]
+    -- run simulation
+    let iters = runSimulationWithIterationAndHorizonE1 initState initInput 60 0.25 20
 
---    let _ = map (`positionOfPlayer` toList states) players
+    -- compute costs per iteration
+    let costs = map totalCostsForPlayersPerIterationE1 iters
+    print $ last costs
 
---    let prox = map (`proximityCost` toList states) players
-
-    -- let totCost = map (\p -> totalCost p states input) players
-    -- let quadrr = quadratizeCostsForPlayerE1 player2 states input
-    -- print $ quadrr
-
-    let iters = runSimulationWithIterationE1 states input 61
-    print $ map (\h -> 180 / pi * (priorState h) ! 2) (last iters)
-
-    -- createAnimationWithIteration players (last iters)
---    putStrLn "Cool Stuff"
+    -- create animation
+    createAnimationOfEquilibriumE (last iters)
--- examples/Example2/Simulation.hs
+++ examples/Example2/Simulation.hs
@@ -8,7 +8,17 @@ import Example.Simulation
 import Example.Utilities
 import TotalCost (totalCost)
 import Numeric.LinearAlgebra
-  
+import Plot.CreateGifs
+
+
+initState :: Vector R
+initState = vector [2.5, -10, pi / 2.0 , 5.0,
+                   -1, -10, pi / 2.0, 5.0,
+                    2.5, 10.0, pi / 2.0, 5.25]
+
+initInput :: Vector R
+initInput = vector [0,0,0,0,0,0]
+
 players :: Floating a => [Player a]
 players = [player1, player2, player3]
 
@@ -18,8 +28,11 @@ quadratizeCostsE1 = quadratizeCosts totalCost players
 quadratizeCostsForPlayerE1 :: Player R -> Vector R -> Vector R -> LinearSystemCosts
 quadratizeCostsForPlayerE1 = quadratizeCostsForPlayer totalCost
 
-runSimulationWithIterationE1 :: Vector R -> Vector R -> Int -> [[StateControlData]]
-runSimulationWithIterationE1 = runSimulationWithIteration totalCost players
+runSimulationWithIterationAndHorizonE1 :: Vector R -> Vector R -> Int -> R -> Int -> [[StateControlData]]
+runSimulationWithIterationAndHorizonE1 = runSimulationWithIterationAndHorizon totalCost players
 
 totalCostsForPlayersPerIterationE1 :: [StateControlData] -> [R]
 totalCostsForPlayersPerIterationE1 = totalCostsForPlayersPerIteration totalCost players
+
+createAnimationOfEquilibriumE :: [StateControlData] -> IO ()
+createAnimationOfEquilibriumE = createAnimationWithIteration "./examples/Example2" players

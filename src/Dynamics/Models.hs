fc5f57805129dca9bba393a3cf1b1b966d7c1057
Ahmed Mogahed <a.mogahed@inginne.com>
2024-11-29 19:50:54 +0200

    Putting assignment of `lf` and `lr` on separate lines

--- src/Dynamics/Models.hs
+++ src/Dynamics/Models.hs
@@ -8,6 +8,7 @@ carDyn _ _ = error "Invalid arguments"
 bicDyn :: Floating a => [a] -> [a] -> [a]
 bicDyn [_, _, psi, v] [a,deltaf] = [v * cos (psi+beta), v * sin (psi+beta), v / lr * sin beta, a]
   where
-    (lf, lr) = (0.5, 0.5)
+    lf = 0.5
+    lr = 0.5
     beta = atan (lr / (lf+lr) * tan deltaf)
 bicDyn _ _ = error "Invalid arguments"
